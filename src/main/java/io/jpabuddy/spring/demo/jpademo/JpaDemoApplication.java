817a4e26957ddabf8565e49e00e9bf277036aefb
Georgii Vlasov <honest.niceman@gmail.com>
2022-05-25 09:36:46 +0400

    Init commit

---
+++ src/main/java/io/jpabuddy/spring/demo/jpademo/JpaDemoApplication.java
@@ -0,0 +1,13 @@
+package io.jpabuddy.spring.demo.jpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jpabuddy/spring/demo/jpademo/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jpabuddy.spring.demo.jpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
